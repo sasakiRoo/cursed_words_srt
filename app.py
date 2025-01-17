8d30796c8e6d982f19f2eacfe2a1f416b33c5a51
sasakiroo <muhammadrobisasaki@gmail.com>
2025-01-18 07:29:50 +0800

    test sub.text

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print("hello")
+import pysrt
+
+listOfWords = ["fuck", "fucking", "shit", "dog", "fuck off"]
+
+subs = pysrt.open("assets/sub.srt")
+
+for sub in subs:
+    # print(f"{sub.index} --> {sub.start} to {sub.end}")
+    arrs = []
+    arrs.append(sub.text)
+    for i in arrs:
+        print(i)
+    print(arrs)
